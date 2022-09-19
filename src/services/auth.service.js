2bda4d7ffbddba1582557d95f17f02fc1e143ae4
Gino Long <ginorenelong@gmail.com>
2022-09-18 21:21:11 -0300

    fixed error handling on auth register

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -13,43 +13,47 @@ import { sendWelcomeMail } from '../services/email.service.js'
 const passportAuthToken = passport.authenticate('jwt', { session: false })
 
 const passportAuthRegister = async (req, res, next) => {
-    if (!req.body.email || !req.body.password) {
-        const error = new Error('Both email and password are necessary to register')
-        error.status = 400
-        throw (error)
-    }
-    passport.authenticate(
-        'register',
-        { session: false },
-        async (err, user, next) => {
-            try {
-
-                // check sequelize validation errors, and format according to error handler
-                if (err) {
-                    const errObj = {}
-                    if (err.errors) {
-                        err.errors.map(er => {
-                            errObj[er.path] = er.message;
-                        })
-                        err.message = errObj
-                        err.status = 400
-                    }
-                    return next(err)
-                }
-
-                // double check for user
-                if (!user) return next(new Error('Something wen\'t wrong with the registration process.'))
-
-                // send welcome email if registration was successful
-                sendWelcomeMail(user.email) 
-
-                // login after successful register
-                passportAuthLogin(req, res, next)
-            } catch (error) {
-                return next(error)
-            }
+    try {
+        if (!req.body.email || !req.body.password) {
+            const error = new Error('Both email and password are necessary to register')
+            error.status = 400
+            throw (error)
         }
-    )(req, res, next)
+        passport.authenticate(
+            'register',
+            { session: false },
+            async (err, user) => {
+                try {
+
+                    // check sequelize validation errors, and format according to error handler
+                    if (err) {
+                        const errObj = {}
+                        if (err.errors) {
+                            err.errors.map(er => {
+                                errObj[er.path] = er.message;
+                            })
+                            err.message = errObj
+                            err.status = 400
+                        }
+                        return next(err)
+                    }
+
+                    // double check for user
+                    if (!user) return next(new Error('Something wen\'t wrong with the registration process.'))
+
+                    // send welcome email if registration was successful
+                    sendWelcomeMail(user.email) 
+
+                    // login after successful register
+                    passportAuthLogin(req, res, next)
+                } catch (error) {
+                    return next(error)
+                }
+            }
+        )(req, res, next)
+    } catch (error) {
+        next(error)
+    }
 }
 
 const passportAuthLogin = async (req, res, next) => {
